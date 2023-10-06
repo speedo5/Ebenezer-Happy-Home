35eefc709e4a15212814f1a49d1e4a4f78d05a4d
unter <109949633+speedo5@users.noreply.github.com>
2023-10-06 18:28:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function showDiv() {
+  // Get a reference to the div element
+  const myDiv = document.getElementById("myDiv");
+
+  // Change the display property to make it visible
+  myDiv.style.display = "block";
+}
+       // Optionally, you can clear the interval after a certain number of iterations
+        // For example, to stop after 10 background changes:
+        // let iterations = 0;
+        // const maxIterations = 10;
+        // const interval = setInterval(() => {
+        //     changeBackground();
+        //     iterations++;
+        //     if (iterations === maxIterations) {
+        //         clearInterv
+    
+        //  </video>al(interval);   }  </div>>
+
+        // }, 5000);   <video autoplay muted loop id="video-background"><div class="overlay">    <source src="backvid.mp4" type="video/mp4"
+
+    
